99c93d5147c5390ec8e469ae333060b78d42905d
thabart <habarthierry@hotmail.fr>
2015-11-16 23:25:18 +0100

    Continue the JWS parser

--- SimpleIdentityServer/SimpleIdentityServer.Core/Jwt/Signature/JwsParser.cs
+++ SimpleIdentityServer/SimpleIdentityServer.Core/Jwt/Signature/JwsParser.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using SimpleIdentityServer.Core.Extensions;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SimpleIdentityServer.Core.Jwt.Signature
@@ -12,6 +14,26 @@ namespace SimpleIdentityServer.Core.Jwt.Signature
     {
         public JwsPayload UnSigned(string jws)
         {
+            var parts = GetParts(jws);
+            if (!parts.Any())
+            {
+                return null;
+            }
+
+            var serializedProtectedHeader = parts[0].Base64Decode();
+            var serializedPayload = parts[1].Base64Decode();
+            var signature = parts[2].Base64Decode();
+
+            var protectedHeader = serializedProtectedHeader.DeserializeWithJavascript<JwsProtectedHeader>();
+            var payload = serializedPayload.DeserializeWithJavascript<JwsPayload>();
+            
+            JwsAlg jwsAlg;
+            if (!Enum.TryParse(protectedHeader.alg, out jwsAlg))
+            {
+                // TODO : maybe throw an exception.
+                return null;
+            }
+
             return null;
         }
 
