8f6a5549422a91abdc792a328e6796af7ca36da0
thabart <thierry.habart@prodatamobility.com>
2015-12-18 09:10:56 +0100

    Ticket 61 : Launch test coverage

--- build.targets.ps1
+++ build.targets.ps1
@@ -6,13 +6,15 @@ $coverageReportDir = "$rootFolder\CodeCoverage\"
 $openCoverExe = "$rootFolder\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe"
 $reportGeneratorExe = "$rootFolder\packages\ReportGenerator.2.3.5.0\tools\ReportGenerator.exe"
 $nunitConsoleExe = "$rootFolder\packages\NUnit.Console.3.0.1\tools\nunit3-console.exe"
+$coverallsExe = "$rootFolder\packages\coveralls.io.1.3.4\tools\coveralls.net.exe"
 
 $utDllPath = "$rootFolder\SimpleIdentityServer.Api.UnitTests\bin\Debug\SimpleIdentityServer.Api.UnitTests.dll"
 $targetDll = "$rootFolder\SimpleIdentityServer.Core\bin\Debug\SimpleIdentityServer.Core.dll"
 
 function RunTestWithCoverage($fullTestDllPaths) {    
-    $arguments =  "-target:""$nunitConsoleExe"" -targetargs:""$utDllPath"" -output:$outputXml -register:user"
-    Start-Process $openCoverExe -ArgumentList $arguments
+    $openCoverArguments =  "-target:""$nunitConsoleExe"" -targetargs:""$utDllPath"" -output:$outputXml -register:user"
+	$coverallsArguments = "--opencover $outputXml --full sources"
+    Start-Process $openCoverExe -ArgumentList $openCoverArguments
 }
 
 RunTestWithCoverage "$utDllPath"
