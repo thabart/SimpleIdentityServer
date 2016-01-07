e4cc02582374b07e9cb478c8aba389b517897b96
thabart <habarthierry@hotmail.fr>
2016-01-07 22:54:28 +0100

    Ticket 71 : Try to fix again the build -_-

--- SimpleIdentityServer/RunUpdatePackage.cmd
+++ SimpleIdentityServer/RunUpdatePackage.cmd
@@ -1,5 +1,7 @@
 @echo off
 rem update the package
 
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+dnvm upgrade
 References\dnx-clr-win-x86.1.0.0-rc1-update1\dnu.cmd restore
 nuget restore
