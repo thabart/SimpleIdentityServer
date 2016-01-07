0aef1f1216a848db512aecfd4943b9d1e9d86e87
thabart <thierry.habart@prodatamobility.com>
2016-01-07 17:41:27 +0100

    Ticket 71 : Move the project to "ASP.NET VNEXT"

---
+++ SimpleIdentityServer/SimpleIdentityServer.Host/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNet.Mvc;
+
+namespace SimpleIdentityServer.Host.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ SimpleIdentityServer/SimpleIdentityServer.Host/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleIdentityServer.Host
+{
+    public class Startup
+    {
+        #region Properties
+
+        public IConfigurationRoot Configuration { get; set; }
+        
+        #endregion
+        
+        #region Public methods
+        
+        public Startup(IHostingEnvironment env, 
+            IApplicationEnvironment appEnv)
+        {
+            // Load all the configuration information from the "json" file & the environment variables.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            
+            System.Console.WriteLine("coucou");
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Purpose of this method is to setup the dependency injection
+            // IServiceCollection interface is responsible for learning about any services that will be supplied to the running application
+            
+            services.AddLogging();
+            
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory)
+        {           
+            loggerFactory.AddConsole();
+             
+            // Specify what frameworks will be used : including MVC, API
+            app.UseIISPlatformHandler(options => options.AuthenticationDescriptions.Clear());
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+        
+        #endregion
+        
+        #region Public static methods
+
+        // Entry point for the application.
+        public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
+        
+        #endregion
+    }
+}
