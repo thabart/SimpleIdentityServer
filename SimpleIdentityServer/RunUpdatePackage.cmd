0b26bd7508055cca1aa01980a6879e5ec8093ba0
thabart <habarthierry@hotmail.fr>
2016-01-07 23:13:49 +0100

    Ticket 71 : Run all the commands in the appveyor file

--- SimpleIdentityServer/RunUpdatePackage.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-rem update the package
-
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
-dnvm upgrade
-dnu restore
-nuget restore
