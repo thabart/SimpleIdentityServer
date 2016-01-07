9c447d5da9add2d9dad984389fd96739e85c93de
thabart <habarthierry@hotmail.fr>
2016-01-07 22:59:07 +0100

    Ticket 71 : Run DNU restore

--- SimpleIdentityServer/References/dnx-clr-win-x86.1.0.0-rc1-update1/dnu.cmd
+++
@@ -1,8 +0,0 @@
-@Echo OFF
-SETLOCAL
-SET ERRORLEVEL=
-
-"%~dp0dnx" %DNX_OPTIONS% "%~dp0lib\Microsoft.Dnx.Tooling\Microsoft.Dnx.Tooling.dll" %*
-
-exit /b %ERRORLEVEL%
-ENDLOCAL
--- SimpleIdentityServer/RunUpdatePackage.cmd
+++ SimpleIdentityServer/RunUpdatePackage.cmd
@@ -3,5 +3,5 @@ rem update the package
 
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 dnvm upgrade
-References\dnx-clr-win-x86.1.0.0-rc1-update1\dnu.cmd restore
+dnu restore
 nuget restore
