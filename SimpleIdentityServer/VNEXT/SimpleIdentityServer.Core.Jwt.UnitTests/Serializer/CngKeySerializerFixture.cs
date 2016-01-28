9ebdcb4b734be10ccfec9b06dd6d5fe8f6fbbc54
thabart <thierry.habart@prodatamobility.com>
2016-01-28 17:55:51 +0100

    Ticket 92 : Migrate one UT to VNEXT

---
+++ SimpleIdentityServer/VNEXT/SimpleIdentityServer.Core.Jwt.UnitTests/Serializer/CngKeySerializerFixture.cs
@@ -0,0 +1,171 @@
+﻿using SimpleIdentityServer.Core.Jwt.Serializer;
+
+using System;
+
+using System.Security.Cryptography;
+using Xunit;
+
+namespace SimpleIdentityServer.Core.Jwt.UnitTests.Serializer
+{
+    public sealed class CngKeySerializerFixture
+    {
+        private ICngKeySerializer _cngKeySerializer;
+
+        #region Serialize with private key
+
+        [Fact]
+        public void When_Passing_Null_To_Serialize_With_Private_Key_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT & ASSERT
+            Assert.Throws<ArgumentNullException>(() => _cngKeySerializer.SerializeCngKeyWithPrivateKey(null));
+        }
+
+        [Fact]
+        public void When_Private_Key_Cannot_Be_Extracted_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+            var keyCreationParameter = new CngKeyCreationParameters
+            {
+                ExportPolicy = CngExportPolicies.None
+            };
+
+            var cnk = CngKey.Create(CngAlgorithm.ECDsaP256, null, keyCreationParameter);
+
+            // ACT & ASSERT
+            Assert.Throws<CryptographicException>(() => _cngKeySerializer.SerializeCngKeyWithPrivateKey(cnk));
+        }
+
+        [Fact]
+        public void When_Serialize_The_CngKey_With_Private_Key_Then_String_Is_Returned()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+            var keyCreationParameter = new CngKeyCreationParameters
+            {
+                ExportPolicy = CngExportPolicies.AllowPlaintextExport
+            };
+
+            var cnk = CngKey.Create(CngAlgorithm.ECDsaP256, null, keyCreationParameter);
+
+            // ACT
+            var result = _cngKeySerializer.SerializeCngKeyWithPrivateKey(cnk);
+
+            // ASSERT
+            Assert.NotNull(result);
+        }
+
+        #endregion
+
+        #region Deserialize with private key
+
+        [Fact]
+        public void When_Passing_Null_To_Deserialize_Function_With_Private_Key_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT & ASSERT
+            Assert.Throws<ArgumentNullException>(() => _cngKeySerializer.DeserializeCngKeyWithPrivateKey(null));
+        }
+
+        [Fact]
+        public void When_Deserialize_Xml_With_Private_Key_Then_CngKey_Is_Returned()
+        {            
+            // ARRANGE
+            InitializeFakeObjects();
+            var keyCreationParameter = new CngKeyCreationParameters
+            {
+                ExportPolicy = CngExportPolicies.AllowPlaintextExport
+            };
+
+            var cnk = CngKey.Create(CngAlgorithm.ECDsaP256, null, keyCreationParameter);
+            var serializedXml = _cngKeySerializer.SerializeCngKeyWithPrivateKey(cnk);
+
+            // ACT
+            var result = _cngKeySerializer.DeserializeCngKeyWithPrivateKey(serializedXml);
+
+            // ASSERT
+            Assert.NotNull(result);
+        }
+
+        #endregion
+
+        #region Serialize with public key
+
+        [Fact]
+        public void When_Passing_Null_To_Serialize_With_Public_Key_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT & ASSERT
+            Assert.Throws<ArgumentNullException>(() => _cngKeySerializer.SerializeCngKeyWithPublicKey(null));
+        }
+
+        [Fact]
+        public void When_Serialize_The_CngKey_With_Public_Key_Then_String_Is_Returned()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+            var keyCreationParameter = new CngKeyCreationParameters
+            {
+                ExportPolicy = CngExportPolicies.AllowPlaintextExport
+            };
+
+            var cnk = CngKey.Create(CngAlgorithm.ECDsaP256, null, keyCreationParameter);
+
+            // ACT
+            var result = _cngKeySerializer.SerializeCngKeyWithPublicKey(cnk);
+
+            // ASSERT
+            Assert.NotNull(result);
+        }
+
+        #endregion
+
+        #region Deserialize with public key
+
+        [Fact]
+        public void When_Passing_Null_Parameter_To_Deserialize_With_Public_Key_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT & ASSERT
+            Assert.Throws<ArgumentNullException>(() => _cngKeySerializer.DeserializeCngKeyWithPublicKey(null));
+        }
+
+        [Fact]
+        public void When_Deserialize_With_Public_Key_Then_String_Is_Returned()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+            
+            var keyCreationParameter = new CngKeyCreationParameters
+            {
+                ExportPolicy = CngExportPolicies.None
+            };
+
+            var cnk = CngKey.Create(CngAlgorithm.ECDsaP521, null, keyCreationParameter);
+
+            var serialized = _cngKeySerializer.SerializeCngKeyWithPublicKey(cnk);
+
+            // ACT
+            var result = _cngKeySerializer.DeserializeCngKeyWithPublicKey(serialized);
+
+            // ASSERT
+            Assert.NotNull(result);
+        }
+
+        #endregion
+
+        private void InitializeFakeObjects()
+        {
+            _cngKeySerializer = new CngKeySerializer();
+        }
+    }
+}
---
+++ SimpleIdentityServer/VNEXT/SimpleIdentityServer.Core.Jwt.UnitTests/Signature/CreateJwsSignatureFixture.cs
@@ -0,0 +1,185 @@
+﻿using Moq;
+using SimpleIdentityServer.Core.Common.Extensions;
+using SimpleIdentityServer.Core.Jwt.Serializer;
+using SimpleIdentityServer.Core.Jwt.Signature;
+
+using System;
+using System.Security;
+using System.Security.Cryptography;
+using Xunit;
+
+namespace SimpleIdentityServer.Core.Jwt.UnitTests.Signature
+{
+    public sealed class CreateJwsSignatureFixture
+    {
+        private Mock<ICngKeySerializer> _cngKeySerializer;
+
+        private ICreateJwsSignature _createJwsSignature;
+
+        #region Rsa algorithm
+
+        #region Create the signature
+
+        [Fact]
+        public void When_Trying_To_Rsa_Sign_With_A_Not_Supported_Algorithm_Then_Null_Is_Returned()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT
+            var result = _createJwsSignature.SignWithRsa(JwsAlg.ES512, string.Empty, string.Empty);
+
+            // ASSERT
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void When_Passing_An_Empty_Serialized_Keys_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT & ASSERT
+            Assert.Throws<ArgumentNullException>(() => _createJwsSignature.SignWithRsa(JwsAlg.RS256, string.Empty, string.Empty));
+        }
+
+        [Fact]
+        public void When_Trying_To_Rsa_Sign_With_Not_Xml_Synthax_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+            var toBeEncrypted = "toBeEncrypted";
+            var serializedKeys = "invalid_serialized_keys";
+
+            // ACT & ASSERT
+           Assert.Throws<XmlSyntaxException>(() => _createJwsSignature.SignWithRsa(JwsAlg.RS256,
+                serializedKeys,
+                toBeEncrypted));
+        }
+
+        [Fact]
+        public void When_Generate_Rsa_Signature_Then_String_Is_Returned()
+        {
+            // ARRANGE
+            const string messageToBeSigned = "message_to_be_signed";
+            InitializeFakeObjects();
+            string serializedKeysXml;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                serializedKeysXml = rsa.ToXmlString(true);
+            }
+
+            // ACT
+            var signedMessage = _createJwsSignature.SignWithRsa(
+                JwsAlg.RS256,
+                serializedKeysXml,
+                messageToBeSigned);
+
+            // ASSERT
+            Assert.NotNull(signedMessage);
+        }
+
+        #endregion
+
+        #region Check the signature
+
+        [Fact]
+        public void When_Trying_To_Check_The_Signature_With_A_Not_Supported_Algorithm_Then_False_Is_Returned()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACT
+            var result = _createJwsSignature.VerifyWithRsa(JwsAlg.ES512, string.Empty, string.Empty, new byte[0]);
+
+            // ASSERT
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void When_Passing_Empty_Serialized_Keys_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+            
+            // ACT & ASSERT
+            Assert.Throws<ArgumentNullException>(() => _createJwsSignature.VerifyWithRsa(JwsAlg.RS256, 
+                string.Empty, 
+                string.Empty,
+                new byte[0]
+                ));
+        }
+
+        [Fact]
+        public void When_Generate_Correct_Rsa_Signature_And_Checking_It_Then_True_Is_Returned()
+        {
+            // ARRANGE
+            const string messageToBeSigned = "message_to_be_signed";
+            InitializeFakeObjects();
+            string serializedKeysXml;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                serializedKeysXml = rsa.ToXmlString(true);
+            }
+
+            var signedMessage = _createJwsSignature.SignWithRsa(JwsAlg.RS256,
+                serializedKeysXml,
+                messageToBeSigned);
+            var signature = signedMessage.Base64DecodeBytes();
+
+            // ACT
+            var isSignatureCorrect = _createJwsSignature.VerifyWithRsa(JwsAlg.RS256,
+                serializedKeysXml,
+                messageToBeSigned,
+                signature);
+
+            // ASSERT
+            Assert.True(isSignatureCorrect);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Elliptic curve algorithm
+
+        #region Generate Signature
+
+        [Fact]
+        public void When_Passing_EmptyString_To_The_Method_Sign_With_EC_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACTS & ASSERTS
+            Assert.Throws<ArgumentNullException>(() => _createJwsSignature.SignWithEllipseCurve(null, null));
+            Assert.Throws<ArgumentNullException>(() => _createJwsSignature.SignWithEllipseCurve("serialized", null));
+        }
+
+        #endregion
+
+        #region Check signature
+
+        [Fact]
+        public void When_Passing_Empty_String_To_The_Method_Check_EC_Signature_Then_Exception_Is_Thrown()
+        {
+            // ARRANGE
+            InitializeFakeObjects();
+
+            // ACTS & ASSERTS
+            Assert.Throws<ArgumentNullException>(() => _createJwsSignature.VerifyWithEllipticCurve(null, null, null));
+            Assert.Throws<ArgumentNullException>(() => _createJwsSignature.VerifyWithEllipticCurve("serialized", null, null));
+            Assert.Throws<ArgumentNullException>(() => _createJwsSignature.VerifyWithEllipticCurve("serialized", "message", null));
+        }
+
+        #endregion
+
+        #endregion
+
+        private void InitializeFakeObjects()
+        {
+            _cngKeySerializer = new Mock<ICngKeySerializer>();
+            _createJwsSignature = new CreateJwsSignature(_cngKeySerializer.Object);   
+        }
+    }
+}
