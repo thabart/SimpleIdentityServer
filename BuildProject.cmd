80a69b92f1cc4fec2c2a945962be69dbb7c5f06e
thabart <thierry.habart@prodatamobility.com>
2016-02-15 18:26:31 +0100

    Ticket 99 : Try to fix the CI & azure

--- BuildProject.cmd
+++ BuildProject.cmd
@@ -1,23 +1,23 @@
-CD SimpleIdentityServer\VNEXT\
+CD SimpleIdentityServer
 
 call powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 
 call dnvm upgrade
 call dnu restore
-dnu build src\SimpleIdentityServer.Host
-dnu build src\SimpleIdentityServer.Core
-dnu build src\SimpleIdentityServer.Core.Jwt
-dnu build src\SimpleIdentityServer.DataAccess.Fake
-dnu build src\SimpleIdentityServer.DataAccess.SqlServer
-dnu build src\SimpleIdentityServer.RateLimitation
-dnu build src\SimpleIdentityServer.Core.Common
-dnu build src\SimpleIdentityServer.Common
+call dnu build src\SimpleIdentityServer.Host
+call dnu build src\SimpleIdentityServer.Core
+call dnu build src\SimpleIdentityServer.Core.Jwt
+call dnu build src\SimpleIdentityServer.DataAccess.Fake
+call dnu build src\SimpleIdentityServer.DataAccess.SqlServer
+call dnu build src\SimpleIdentityServer.RateLimitation
+call dnu build src\SimpleIdentityServer.Core.Common
+call dnu build src\SimpleIdentityServer.Common
 
-dnu pack src\SimpleIdentityServer.Common -out nuget\
-dnu pack src\SimpleIdentityServer.Core.Common -out nuget\
-dnu pack src\SimpleIdentityServer.RateLimitation -out nuget\
-dnu pack src\SimpleIdentityServer.DataAccess.SqlServer -out nuget\
-dnu pack src\SimpleIdentityServer.DataAccess.Fake -out nuget\
-dnu pack src\SimpleIdentityServer.Core.Jwt -out nuget\
-dnu pack src\SimpleIdentityServer.Core -out nuget\
-dnu pack src\SimpleIdentityServer.Host -out nuget\
+call dnu pack src\SimpleIdentityServer.Common --out nuget\
+call dnu pack src\SimpleIdentityServer.Core.Common --out nuget\
+call dnu pack src\SimpleIdentityServer.RateLimitation --out nuget\
+call dnu pack src\SimpleIdentityServer.DataAccess.SqlServer --out nuget\
+call dnu pack src\SimpleIdentityServer.DataAccess.Fake --out nuget\
+call dnu pack src\SimpleIdentityServer.Core.Jwt --out nuget\
+call dnu pack src\SimpleIdentityServer.Core --out nuget\
+call dnu pack src\SimpleIdentityServer.Host --out nuget\
--- SimpleIdentityServer/out/approot/src/SimpleIdentityServer.Common/IModuleRegister.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SimpleIdentityServer.Common
-{
-    public interface IModuleRegister
-    {
-        void RegisterType<TFrom, TTo>() where TTo : TFrom;
-
-        void RegisterInstance<TFrom>(TFrom obj);
-    }
-}
