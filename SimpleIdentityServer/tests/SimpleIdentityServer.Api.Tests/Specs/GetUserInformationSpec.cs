8a0f123e59f74c37f973d7a2c484adba366615af
thabart <habarthierry@hotmail.fr>
2016-03-05 22:36:48 +0100

    Ticket 117 : Fix GetUserInformation.feature

--- SimpleIdentityServer/tests/SimpleIdentityServer.Api.Tests/Specs/GetUserInformationSpec.cs
+++ SimpleIdentityServer/tests/SimpleIdentityServer.Api.Tests/Specs/GetUserInformationSpec.cs
@@ -1,28 +1,46 @@
-﻿using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Web;
-using System.Web.Http;
-using SimpleIdentityServer.Host.DTOs.Request;
+﻿#region copyright
+// Copyright 2015 Habart Thierry
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using Microsoft.Extensions.DependencyInjection;
 using SimpleIdentityServer.Api.Tests.Common;
 using SimpleIdentityServer.Api.Tests.Common.Fakes;
+using SimpleIdentityServer.Api.Tests.Common.Fakes.Models;
+using SimpleIdentityServer.Api.Tests.Extensions;
+using SimpleIdentityServer.Core.Common.Extensions;
+using SimpleIdentityServer.Core.Configuration;
+using SimpleIdentityServer.Core.Helpers;
 using SimpleIdentityServer.Core.Jwt;
+using SimpleIdentityServer.DataAccess.Fake.Extensions;
+using SimpleIdentityServer.Host.DTOs.Request;
 using SimpleIdentityServer.RateLimitation.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
 using Xunit;
 using MODELS = SimpleIdentityServer.DataAccess.Fake.Models;
-using SimpleIdentityServer.Core.Configuration;
-using SimpleIdentityServer.Core.Common.Extensions;
-using System.Collections.Generic;
-using Microsoft.Extensions.DependencyInjection;
-using SimpleIdentityServer.DataAccess.Fake.Extensions;
-using SimpleIdentityServer.Core.Helpers;
-using System.Security.Cryptography;
-using SimpleIdentityServer.Api.Tests.Common.Fakes.Models;
-using SimpleIdentityServer.Api.Tests.Extensions;
-using System.Linq;
-using System;
 
 namespace SimpleIdentityServer.Api.Tests.Specs
 {
@@ -31,13 +49,11 @@ namespace SimpleIdentityServer.Api.Tests.Specs
     {
         private ISecurityHelper _securityHelper;
         
-        private HttpResponseMessage _authorizationResponseMessage;
+        private ConfiguredTaskAwaitable<HttpResponseMessage> _authorizationResponseMessage;
 
-        private HttpResponseMessage _userInformationResponseMessage;
+        private ConfiguredTaskAwaitable<HttpResponseMessage> _userInfoResponseMessage;
 
         private FakeSimpleIdentityServerConfigurator _fakeSimpleIdentityServerConfigurator;
-
-        private JwsPayload _jwsPayload;
 
         private GlobalContext _globalContext;
 
@@ -59,8 +75,9 @@ namespace SimpleIdentityServer.Api.Tests.Specs
                services.AddInstance<ISimpleIdentityServerConfigurator>(_fakeSimpleIdentityServerConfigurator);
             });
         }
-        
-        
+
+        #region Given
+
         [Given("a mobile application (.*) is defined")]
         public void GivenClient(string clientId)
         {
@@ -284,13 +301,7 @@ namespace SimpleIdentityServer.Api.Tests.Specs
 
             _globalContext.FakeDataSource.Consents.Add(consent);
         }
-
-        private MODELS.Client GetClient(string clientId)
-        {
-            var client = _globalContext.FakeDataSource.Clients.SingleOrDefault(c => c.ClientId == clientId);
-            return client;
-        }
-
+        
         [Given("create a resource owner")]
         public void GivenCreateAResourceOwner(Table table)
         {
@@ -338,26 +349,30 @@ namespace SimpleIdentityServer.Api.Tests.Specs
                 authorizationRequest.state,
                 authorizationRequest.nonce,
                 authorizationRequest.claims);
-            _authorizationResponseMessage = httpClient.GetAsync(url).Result;
+            _authorizationResponseMessage = httpClient.GetAsync(url).ConfigureAwait(false);
         }
 
+        #endregion
+
+        #region When
+
         [When("requesting user information and the access token is passed to the authorization header")]
-        public void WhenRequestingUserInformationAndPassedTheAccessTokenToAuthorizationHeader()
+        public async Task WhenRequestingUserInformationAndPassedTheAccessTokenToAuthorizationHeader()
         {
-            var query = HttpUtility.ParseQueryString(_authorizationResponseMessage.Headers.Location.Fragment.TrimStart('#'));
+            var authorizationResponseMessage = await _authorizationResponseMessage;
+            var query = HttpUtility.ParseQueryString(authorizationResponseMessage.Headers.Location.Fragment.TrimStart('#'));
             var accessToken = query["access_token"];
             var httpConfiguration = new HttpConfiguration();
             var httpClient = _globalContext.TestServer.CreateClient();
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            _userInformationResponseMessage = httpClient.GetAsync("/userinfo").Result;
-            var r = _userInformationResponseMessage.Content.ReadAsStringAsync().Result;
-            _jwsPayload = _userInformationResponseMessage.Content.ReadAsAsync<JwsPayload>().Result;
+            _userInfoResponseMessage = httpClient.GetAsync("/userinfo").ConfigureAwait(false);
         }
 
         [When("requesting user information and the access token is passed to the HTTP body")]
-        public void WhenRequestingUserInformationAndPassedTheAccessTokenToHttpBody()
+        public async Task WhenRequestingUserInformationAndPassedTheAccessTokenToHttpBody()
         {
-            var query = HttpUtility.ParseQueryString(_authorizationResponseMessage.Headers.Location.Fragment.TrimStart('#'));
+            var authorizationResponseMessage = await _authorizationResponseMessage;
+            var query = HttpUtility.ParseQueryString(authorizationResponseMessage.Headers.Location.Fragment.TrimStart('#'));
             var accessToken = query["access_token"];
             var httpConfiguration = new HttpConfiguration();
             var httpClient = _globalContext.TestServer.CreateClient();
@@ -369,50 +384,57 @@ namespace SimpleIdentityServer.Api.Tests.Specs
                 }
             };
             var encodedParameter = new FormUrlEncodedContent(parameter);
-            _userInformationResponseMessage = httpClient.PostAsync("/userinfo", encodedParameter).Result;
-            var r = _userInformationResponseMessage.Content.ReadAsStringAsync().Result;
-            _jwsPayload = _userInformationResponseMessage.Content.ReadAsAsync<JwsPayload>().Result;
+            _userInfoResponseMessage = httpClient.PostAsync("/userinfo", encodedParameter).ConfigureAwait(false);
         }
 
         [When("requesting user information and the access token is passed in the query string")]
-        public void WhenRequestingUserInformationAndPassedTheAccessTokenInTheQueryString()
+        public async Task WhenRequestingUserInformationAndPassedTheAccessTokenInTheQueryString()
         {
-            var query = HttpUtility.ParseQueryString(_authorizationResponseMessage.Headers.Location.Fragment.TrimStart('#'));
+            var authorizationResponseMessage = await _authorizationResponseMessage;
+            var query = HttpUtility.ParseQueryString(authorizationResponseMessage.Headers.Location.Fragment.TrimStart('#'));
             var accessToken = query["access_token"];
             var httpClient = _globalContext.TestServer.CreateClient();
-            _userInformationResponseMessage = httpClient
+            _userInfoResponseMessage = httpClient
                 .GetAsync(string.Format("/userinfo?access_token={0}", accessToken))
-                .Result;
-            var r = _userInformationResponseMessage.Content.ReadAsStringAsync().Result;
-            _jwsPayload = _userInformationResponseMessage.Content.ReadAsAsync<JwsPayload>().Result;
+                .ConfigureAwait(false);
         }
 
+        #endregion
+
+        #region Then
+
         [Then("HTTP status code is (.*)")]
-        public void ThenHttpStatusCodeIs(HttpStatusCode httpStatusCode)
+        public async Task ThenHttpStatusCodeIs(HttpStatusCode httpStatusCode)
         {
-            Assert.True(_userInformationResponseMessage.StatusCode.Equals(httpStatusCode));
+            var userInformationResponseMessage = await _userInfoResponseMessage;
+            Assert.True(userInformationResponseMessage.StatusCode.Equals(httpStatusCode));
         }
 
         [Then("the claim (.*) with value (.*) is returned by the JWS payload")]
-        public void ThenTheClaimWithValueIsReturnedByJwsPayLoad(string claimName, string val)
+        public async Task ThenTheClaimWithValueIsReturnedByJwsPayLoad(string claimName, string val)
         {
-            var claimValue = _jwsPayload.GetClaimValue(claimName);
-
+            var userInfoResponseMessage = await _userInfoResponseMessage;
+            var jwsPayload = await userInfoResponseMessage.Content.ReadAsAsync<JwsPayload>().ConfigureAwait(false);
+            var claimValue = jwsPayload.GetClaimValue(claimName);
             Assert.NotNull(claimValue);
             Assert.True(claimValue.Equals(val));
         }
 
         [Then("the JWS payload contains (.*) claims")]
-        public void ThenTheJwsPayloadContainsNumberOfClaims(int numberOfClaims)
+        public async Task ThenTheJwsPayloadContainsNumberOfClaims(int numberOfClaims)
         {
-            Assert.True(_jwsPayload.Count.Equals(numberOfClaims));
+            var userInfoResponseMessage = await _userInfoResponseMessage;
+            var jwsPayload = await userInfoResponseMessage.Content.ReadAsAsync<JwsPayload>().ConfigureAwait(false);
+            Assert.True(jwsPayload.Count.Equals(numberOfClaims));
         }
 
         [Then("the returned address is")]
-        public void ThenTheReturnedAddressIs(Table table)
+        public async Task ThenTheReturnedAddressIs(Table table)
         {
+            var userInfoResponseMessage = await _userInfoResponseMessage;
+            var jwsPayload = await userInfoResponseMessage.Content.ReadAsAsync<JwsPayload>().ConfigureAwait(false);
             var address = table.CreateInstance<MODELS.Address>();
-            var claimValue = _jwsPayload.GetClaimValue("address");
+            var claimValue = jwsPayload.GetClaimValue("address");
             Assert.NotNull(claimValue);
             var receivedAddress = claimValue.DeserializeWithDataContract<MODELS.Address>();
             Assert.NotNull(receivedAddress);
@@ -422,6 +444,14 @@ namespace SimpleIdentityServer.Api.Tests.Specs
             Assert.True(receivedAddress.Locality.Equals(address.Locality));
             Assert.True(receivedAddress.Region.Equals(address.Region));
             Assert.True(receivedAddress.StreetAddress.Equals(address.StreetAddress));
+        }
+
+        #endregion
+
+        private MODELS.Client GetClient(string clientId)
+        {
+            var client = _globalContext.FakeDataSource.Clients.SingleOrDefault(c => c.ClientId == clientId);
+            return client;
         }
     }
 }
