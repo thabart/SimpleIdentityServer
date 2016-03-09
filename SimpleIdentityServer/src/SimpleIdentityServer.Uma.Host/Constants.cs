d6183727184ea0aa170ae8a567ea12d02c6686b7
thabart <habarthierry@hotmail.fr>
2016-03-09 22:25:08 +0100

    Ticket 116 : Add the SimpleIdentityServer.Uma

---
+++ SimpleIdentityServer/src/SimpleIdentityServer.Uma.Host/Constants.cs
@@ -0,0 +1,92 @@
+﻿#region copyright
+// Copyright 2015 Habart Thierry
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+namespace SimpleIdentityServer.Uma.Host
+{
+    public static class Constants
+    {
+        public static class RouteValues
+        {
+            public const string Configuration = ".well-known/uma-configuration";
+
+            public const string ResourceSet = "rs/resource_set";
+        }
+
+        public static class ConfigurationResponseNames
+        {
+            public const string Version = "version";
+
+            public const string Issuer = "issuer";
+
+            public const string PatProfilesSupported = "pat_profiles_supported";
+
+            public const string AatProfilesSupported = "aat_profiles_supported";
+
+            public const string RtpProfilesSupported = "rpt_profiles_supported";
+
+            public const string PatGrantTypesSupported = "pat_grant_types_supported";
+
+            public const string AatGrantTypesSupported = "aat_grant_types_supported";
+
+            public const string ClaimTokenProfilesSupported = "claim_token_profiles_supported";
+
+            public const string UmaProfilesSupported = "uma_profiles_supported";
+
+            public const string DynamicClientEndPoint = "dynamic_client_endpoint";
+
+            public const string TokenEndPoint = "token_endpoint";
+
+            public const string AuthorizationEndPoint = "authorization_endpoint";
+
+            public const string RequestingPartyClaimsEndPoint = "requesting_party_claims_endpoint";
+
+            public const string IntrospectionEndPoint = "introspection_endpoint";
+
+            public const string ResourceSetRegistrationEndPoint = "resource_set_registration_endpoint";
+
+            public const string PermissionRegistrationEndPoint = "permission_registration_endpoint";
+
+            public const string RtpEndPoint = "rpt_endpoint";
+        }
+
+        public static class ResourceSetResponseNames
+        {
+            public const string Id = "_id";
+
+            public const string Name = "name";
+
+            public const string Uri = "uri";
+
+            public const string Type = "type";
+
+            public const string Scopes = "scopes";
+
+            public const string IconUri = "icon_uri";
+        }
+
+        public static class AddResourceSetResponseNames
+        {
+            public const string Id = "_id";
+
+            public const string UserAccessPolicyUri = "user_access_policy_uri";
+        }
+
+        public static class UpdateSetResponseNames
+        {
+            public const string Id = "_id";
+        }
+    }
+}
---
+++ SimpleIdentityServer/src/SimpleIdentityServer.Uma.Host/DTOs/Responses/ResourceSetResponse.cs
@@ -0,0 +1,43 @@
+﻿#region copyright
+// Copyright 2015 Habart Thierry
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace SimpleIdentityServer.Uma.Host.DTOs.Responses
+{
+    [DataContract]
+    public class ResourceSetResponse
+    {
+        [DataMember(Name = Constants.ResourceSetResponseNames.Id)]
+        public string Id { get; set; }
+
+        [DataMember(Name = Constants.ResourceSetResponseNames.Name)]
+        public string Name { get; set; }
+
+        [DataMember(Name = Constants.ResourceSetResponseNames.Uri)]
+        public string Uri { get; set; }
+
+        [DataMember(Name = Constants.ResourceSetResponseNames.Type)]
+        public string Type { get; set; }
+
+        [DataMember(Name = Constants.ResourceSetResponseNames.Scopes)]
+        public List<string> Scopes { get; set; }
+
+        [DataMember(Name = Constants.ResourceSetResponseNames.IconUri)]
+        public string IconUri { get; set; }
+    }
+}
