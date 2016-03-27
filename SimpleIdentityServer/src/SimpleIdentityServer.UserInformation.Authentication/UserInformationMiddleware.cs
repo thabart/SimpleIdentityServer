15ec244c42dfe0dda81e81a800aab7080c2e0b8d
thabart <habarthierry@hotmail.fr>
2016-03-27 19:20:21 +0200

    Ticket 129 : Redirect to the open-id contract

--- SimpleIdentityServer/src/SimpleIdentityServer.UserInformation.Authentication/UserInformationMiddleware.cs
+++ SimpleIdentityServer/src/SimpleIdentityServer.UserInformation.Authentication/UserInformationMiddleware.cs
@@ -24,6 +24,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Threading.Tasks;
 #if DNXCORE50
