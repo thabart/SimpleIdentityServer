8d8564425069e2c3aa0c5ea653a0c281b2cf1049
thabart <habarthierry@hotmail.fr>
2016-03-30 18:00:25 +0200

    Ticket 5 : Update nuget package

--- BuildProject.cmd
+++ BuildProject.cmd
@@ -4,20 +4,26 @@ call powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{iex ((new-o
 
 call dnvm upgrade
 call dnu restore
+call dnu build src\System.Security.Cryptography.Algorithms.Extensions
+call dnu build src\SimpleIdentityServer.UserInformation.Authentication
+call dnu build src\SimpleIdentityServer.RateLimitation
+call dnu build src\SimpleIdentityServer.Oauth2Instrospection.Authentication
 call dnu build src\SimpleIdentityServer.Host
 call dnu build src\SimpleIdentityServer.Core
-call dnu build src\SimpleIdentityServer.Core.Jwt
+call dnu build src\SimpleIdentityServer.Logging
 call dnu build src\SimpleIdentityServer.DataAccess.Fake
 call dnu build src\SimpleIdentityServer.DataAccess.SqlServer
-call dnu build src\SimpleIdentityServer.RateLimitation
-call dnu build src\SimpleIdentityServer.Core.Common
+call dnu build src\SimpleIdentityServer.Core.Jwt
 call dnu build src\SimpleIdentityServer.Common
 
-call dnu pack src\SimpleIdentityServer.Common --out nuget\
-call dnu pack src\SimpleIdentityServer.Core.Common --out nuget\
+call dnu pack src\System.Security.Cryptography.Algorithms.Extensions --out nuget\
+call dnu pack src\SimpleIdentityServer.UserInformation.Authentication --out nuget\
 call dnu pack src\SimpleIdentityServer.RateLimitation --out nuget\
-call dnu pack src\SimpleIdentityServer.DataAccess.SqlServer --out nuget\
-call dnu pack src\SimpleIdentityServer.DataAccess.Fake --out nuget\
-call dnu pack src\SimpleIdentityServer.Core.Jwt --out nuget\
-call dnu pack src\SimpleIdentityServer.Core --out nuget\
+call dnu pack src\SimpleIdentityServer.Oauth2Instrospection.Authentication --out nuget\
 call dnu pack src\SimpleIdentityServer.Host --out nuget\
+call dnu pack src\SimpleIdentityServer.Core --out nuget\
+call dnu pack src\SimpleIdentityServer.Logging --out nuget\
+call dnu pack src\SimpleIdentityServer.DataAccess.Fake --out nuget\
+call dnu pack src\SimpleIdentityServer.DataAccess.SqlServer --out nuget\
+call dnu pack src\SimpleIdentityServer.Core.Jwt --out nuget\
+call dnu pack src\SimpleIdentityServer.Common --out nuget\
