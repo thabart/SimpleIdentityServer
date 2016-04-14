53b421b6f814ce33a169755a3af25b8d395dfd0e
Thierry Habart <habarthierry@hotmail.fr>
2016-04-14 16:24:32 +0200

    Ticket 106 : Add friend assembly

--- SimpleIdentityServer/src/SimpleIdentityServer.Core/Properties/AssemblyInfo.cs
+++ SimpleIdentityServer/src/SimpleIdentityServer.Core/Properties/AssemblyInfo.cs
@@ -49,3 +49,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("SimpleIdentityServer.Core.UnitTests")]
