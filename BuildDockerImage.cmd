e87f355457537764d924bf268263e4f191b7e5af
thabart <habarthierry@hotmail.fr>
2016-06-02 19:36:50 +0200

    Update connection string

--- BuildDockerImage.cmd
+++ BuildDockerImage.cmd
@@ -11,3 +11,9 @@ docker-machine start dev
 docker build -t simpleidserver -f Dockerfile-Authorization .
 docker run -t -d -p 5000:5000 simpleidserver
 docker run -t -i simpleidserver /bin/bash
+
+# Run postgresql
+docker run --name postgresql -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=postgres -d postgres
+
+# Run simpleidserver
+docker run --name simpleidserver -e DB_ALIAS=postgresql -e DB_PORT=5432 --link postgresql -it simpleidserver /bin/bash
