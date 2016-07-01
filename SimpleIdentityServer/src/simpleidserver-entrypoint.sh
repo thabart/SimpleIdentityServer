37ca9253b7994e8efbd87fda6d2e74e39a051c48
thabart <habarthierry@hotmail.fr>
2016-07-01 16:39:51 +0200

    Update SH

--- SimpleIdentityServer/src/simpleidserver-entrypoint.sh
+++ SimpleIdentityServer/src/simpleidserver-entrypoint.sh
@@ -2,5 +2,4 @@
 
 ./wait-for-it.sh $DB_ALIAS:$DB_PORT
 bash -c "cd SimpleIdentityServer.DataAccess.SqlServer && dotnet ef database update"
-nohup dotnet run --project SimpleIdentityServer.Startup/project.json --server.urls=http://*:5000 & 
-dotnet run --project SimpleIdentityServer.Startup/project.json --server.urls=https://*:5443
+dotnet run --project SimpleIdentityServer.Startup/project.json --server.urls=http://*:5000 & dotnet run --project SimpleIdentityServer.Startup/project.json --server.urls=https://*:5443
--- SimpleIdentityServer/src/uma-entrypoint.sh
+++ SimpleIdentityServer/src/uma-entrypoint.sh
@@ -2,5 +2,4 @@
 
 ./wait-for-it.sh $DB_ALIAS:$DB_PORT
 bash -c "cd SimpleIdentityServer.Uma.EF && dotnet ef database update"
-nohup dotnet run --project SimpleIdentityServer.Uma.Host/project.json --server.urls=http://*:5001 &
-dotnet run --project SimpleIdentityServer.Uma.Host/project.json --server.urls=https://*:5445
+dotnet run --project SimpleIdentityServer.Uma.Host/project.json --server.urls=http://*:5001 & dotnet run --project SimpleIdentityServer.Uma.Host/project.json --server.urls=https://*:5445
