15a61f39e110939bbe8faf7241c0f36e162ef8af
thabart <habarthierry@hotmail.fr>
2016-07-01 12:04:52 +0200

    Update sh files

--- SimpleIdentityServer/src/simpleidserver-entrypoint.sh
+++ SimpleIdentityServer/src/simpleidserver-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./wait-for-it.sh $DB_ALIAS:$DB_PORT && bash -c "cd SimpleIdentityServer.DataAccess.SqlServer && dotnet ef database update" && dotnet run --project SimpleIdentityServer.Startup/project.json --server.urls=http://*:5000
+./wait-for-it.sh $DB_ALIAS:$DB_PORT && bash -c "cd SimpleIdentityServer.DataAccess.SqlServer && dotnet ef database update" && dotnet run --project SimpleIdentityServer.Startup/project.json --server.urls=http://*:5000;https://*:5443
