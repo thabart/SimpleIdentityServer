d2135b88bd35e275b59629dfa8cfd7c19d554a1d
thabart <habarthierry@hotmail.fr>
2016-07-03 17:12:57 +0200

    Update UMA sh

--- SimpleIdentityServer/src/uma-http-entrypoint.sh
+++ SimpleIdentityServer/src/uma-http-entrypoint.sh
@@ -2,4 +2,4 @@
 
 ./wait-for-it.sh $DB_ALIAS:$DB_PORT
 bash -c "cd SimpleIdentityServer.Uma.EF && dotnet ef database update"
-dotnet run --project SimpleIdentityServer.Uma.Host/project.json --server.urls=http://*:5000
+dotnet run --project SimpleIdentityServer.Uma.Host/project.json --server.urls=http://*:5001
