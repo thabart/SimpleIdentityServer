25a33e87d4cf810174b53663c0ab21c869d632c1
thabart <habarthierry@hotmail.fr>
2016-07-28 11:46:57 +0200

    fix launcher

--- SimpleIdentityServer/launch.cmd
+++ SimpleIdentityServer/launch.cmd
@@ -1,6 +1,6 @@
 REM for /d /r . %%d in (bin,obj) do @if exist "%%d" rd /s/q "%%d"
 START cmd /k "cd src/SimpleIdentityServer.Startup && dotnet run -f net46 --server.urls=https://*:5443"
-START cmd /k "bash ./wait-for-it.sh localhost:5443 -t 300 && cd src/SimpleIdentityServer.Uma.Host && dotnet run -f net46 --server.urls=https://*:5445"
-START cmd /k "bash ./wait-for-it.sh localhost:5445 -t 300 && cd src/SimpleIdentityServer.Manager.Host.Startup && dotnet run -f net46 --server.urls=http://*:5002"
-START cmd /k "bash ./wait-for-it.sh localhost:5002 -t 300 && cd src/SimpleIdentityServer.Configuration.Startup && dotnet run -f net46 --server.urls=http://*:5004"
+START cmd /k "bash ./src/wait-for-it.sh localhost:5443 -t 300 && cd src/SimpleIdentityServer.Uma.Host && dotnet run -f net46 --server.urls=https://*:5445"
+START cmd /k "bash ./src/wait-for-it.sh localhost:5445 -t 300 && cd src/SimpleIdentityServer.Manager.Host.Startup && dotnet run -f net46 --server.urls=http://*:5002"
+START cmd /k "bash ./src/wait-for-it.sh localhost:5002 -t 300 && cd src/SimpleIdentityServer.Configuration.Startup && dotnet run -f net46 --server.urls=http://*:5004"
 echo Applications are running ...
