e95aa6395ab022085e1b74cb27178dd310ec9334
thabart <habarthierry@hotmail.fr>
2016-08-31 21:35:36 +0200

    Update the doc

---
+++ azure-docker.cmd
@@ -0,0 +1,6 @@
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://104.42.109.50:2376"
+export DOCKER_CERT_PATH="C:\Users\thabart\.docker\machine\machines\machine"
+export DOCKER_MACHINE_NAME="machine"
+# Run this command to configure your shell:
+# eval $("C:\Program Files\Docker Toolbox\docker-machine.exe" env machine)
