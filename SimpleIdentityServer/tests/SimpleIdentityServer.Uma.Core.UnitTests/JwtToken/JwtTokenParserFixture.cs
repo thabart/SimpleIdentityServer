7ddcd73686e757f267d96ddedb48e7fbf70f9174
thabart <habarthierry@hotmail.fr>
2016-11-30 20:33:22 +0100

    Ticket 234 : Fix the UT

--- SimpleIdentityServer/tests/SimpleIdentityServer.Uma.Core.UnitTests/JwtToken/JwtTokenParserFixture.cs
+++ SimpleIdentityServer/tests/SimpleIdentityServer.Uma.Core.UnitTests/JwtToken/JwtTokenParserFixture.cs
@@ -16,6 +16,7 @@
 
 using Moq;
 using SimpleIdentityServer.Client;
+using SimpleIdentityServer.Core.Common.DTOs;
 using SimpleIdentityServer.Core.Jwt;
 using SimpleIdentityServer.Core.Jwt.Converter;
 using SimpleIdentityServer.Core.Jwt.Signature;
