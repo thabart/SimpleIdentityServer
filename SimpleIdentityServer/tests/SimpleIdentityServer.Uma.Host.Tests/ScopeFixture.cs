2e935cc72b8fa526be0ed4ba231dee87133cf408
thabart <habarthierry@hotmail.fr>
2016-12-16 10:33:09 +0100

    Ticket 236 : Finish to UT the scope client

--- SimpleIdentityServer/tests/SimpleIdentityServer.Uma.Host.Tests/ScopeFixture.cs
+++ SimpleIdentityServer/tests/SimpleIdentityServer.Uma.Host.Tests/ScopeFixture.cs
@@ -62,6 +62,80 @@ namespace SimpleIdentityServer.Uma.Host.Tests
             Assert.True(scope.Name == "add client");
         }
 
+        [Fact]
+        public async Task When_Removing_Scope_Then_It_Cannot_Be_Retrieved()
+        {
+            const string baseUrl = "http://localhost:5000";
+            // ARRANGE
+            InitializeFakeObjects();
+            _httpClientFactoryStub.Setup(h => h.GetHttpClient()).Returns(_server.Client);
+
+            // ACT
+            var addResult = await _scopeClient.AddByResolution(new PostScope
+            {
+                Id = "remove_client",
+                Name = "remove client",
+                IconUri = "http://localhost/client.png"
+            }, baseUrl + "/.well-known/uma-configuration");
+            var isRemoved = await _scopeClient.DeleteByResolution(addResult.Id, baseUrl + "/.well-known/uma-configuration");
+            var ex = await Assert.ThrowsAsync<HttpRequestException>(() => _scopeClient.GetByResolution(addResult.Id, baseUrl + "/.well-known/uma-configuration"));
+
+            // ASSERTS
+            Assert.True(isRemoved);
+            Assert.NotNull(ex);
+        }
+
+        [Fact]
+        public async Task When_Retrieving_Scopes_Then_Identifiers_Are_Returned()
+        {
+            const string baseUrl = "http://localhost:5000";
+            // ARRANGE
+            InitializeFakeObjects();
+            _httpClientFactoryStub.Setup(h => h.GetHttpClient()).Returns(_server.Client);
+
+            // ACT
+            var addResult = await _scopeClient.AddByResolution(new PostScope
+            {
+                Id = "other_client",
+                Name = "other client",
+                IconUri = "http://localhost/client.png"
+            }, baseUrl + "/.well-known/uma-configuration");
+            var identifiers = await _scopeClient.GetAllByResolution(baseUrl + "/.well-known/uma-configuration");
+
+            // ASSERTS
+            Assert.NotNull(identifiers);
+            Assert.True(identifiers.Any(i => i == addResult.Id));
+        }
+
+        [Fact]
+        public async Task When_Updating_Scope_Then_Information_Are_Persisted()
+        {
+            const string baseUrl = "http://localhost:5000";
+            // ARRANGE
+            InitializeFakeObjects();
+            _httpClientFactoryStub.Setup(h => h.GetHttpClient()).Returns(_server.Client);
+
+            // ACT
+            var addResult = await _scopeClient.AddByResolution(new PostScope
+            {
+                Id = "add_customer",
+                Name = "add customer",
+                IconUri = "http://localhost/customer.png"
+            }, baseUrl + "/.well-known/uma-configuration");
+            var updateResult = await _scopeClient.UpdateByResolution(new PutScope
+            {
+                Id = addResult.Id,
+                Name = "modified customer",
+                IconUri = "http://localhost/modifier_customer.png"
+            }, baseUrl + "/.well-known/uma-configuration");
+            var information = await _scopeClient.GetByResolution(addResult.Id, baseUrl + "/.well-known/uma-configuration");
+
+            // ASSERTS
+            Assert.NotNull(information);
+            Assert.True(information.IconUri == "http://localhost/modifier_customer.png");
+            Assert.True(information.Name == "modified customer");
+        }
+
         private void InitializeFakeObjects()
         {
             _httpClientFactoryStub = new Mock<IHttpClientFactory>();
