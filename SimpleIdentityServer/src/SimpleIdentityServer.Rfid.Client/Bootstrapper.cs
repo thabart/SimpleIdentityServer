f8bc97ccdc634fecb12e8666c79fa14ec8f351cd
thabart <habarthierry@hotmail.fr>
2017-01-11 16:04:14 +0100

    Add small prism application to validate the card

---
+++ SimpleIdentityServer/src/SimpleIdentityServer.Rfid.Client/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using Prism.Unity;
+using Microsoft.Practices.ServiceLocation;
+using Prism.Modularity;
+using SimpleIdentityServer.Rfid.Home;
+
+namespace SimpleIdentityServer.Rfid.Client
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<Shell>();
+        }
+                                                                                                                 
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            moduleCatalog.AddModule(typeof(RfidHomeModule));
+        }
+    }
+}
---
+++ SimpleIdentityServer/src/SimpleIdentityServer.Rfid.Home/RfidHomeModule.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using SimpleIdentityServer.Rfid.Home.Views;
+
+namespace SimpleIdentityServer.Rfid.Home
+{
+    public class RfidHomeModule : IModule
+    {
+        private readonly IRegionViewRegistry _registry;
+
+        public RfidHomeModule(IRegionViewRegistry registry)
+        {
+            _registry = registry;
+        }
+
+        public void Initialize()
+        {
+            _registry.RegisterViewWithRegion("MainRegion", typeof(HomeView));
+        }
+    }
+}
