bc89b85b39a4473ab192c526b52709d623aa0631
thabart <habarthierry@hotmail.fr>
2017-03-14 19:39:23 +0100

    Ticket 244 : Add migration script

---
+++ SimpleIdentityServer/src/SimpleIdentityServer.EventStore.EF/Migrations/20170314183656_Initialize.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleIdentityServer.EventStore.EF.Migrations
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    AggregateId = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Payload = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_events", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events");
+        }
+    }
+}
