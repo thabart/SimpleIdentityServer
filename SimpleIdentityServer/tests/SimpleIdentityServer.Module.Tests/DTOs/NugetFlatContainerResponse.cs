c0049d9cce2ea60a0c2ee0ae50c57b9d6d28f002
thabart <habarthierry@hotmail.fr>
2018-05-27 21:45:57 +0200

    Add a method to restore the configuration file

---
+++ SimpleIdentityServer/tests/SimpleIdentityServer.Module.Tests/DTOs/NugetFlatContainerResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace SimpleIdentityServer.Module.Tests.DTOs
+{
+    [DataContract]
+    public class NugetFlatContainerResponse
+    {
+        [DataMember(Name = "versions")]
+        public List<string> Versions { get; set; }
+    }
+}
---
+++ SimpleIdentityServer/tests/SimpleIdentityServer.Module.Tests/NugetSpecification.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace SimpleIdentityServer.Module.Tests
+{
+    [Serializable]
+    public class NugetDependency
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+        [XmlAttribute("version")]
+        public string Version { get; set; }
+    }
+
+    [Serializable]
+    public class NugetGroup
+    {
+        [XmlAttribute("targetFramework")]
+        public string TargetFramework { get; set; }
+        [XmlElement("dependency")]
+        public List<NugetDependency> Dependencies { get; set; }
+    }
+
+    [Serializable]
+    public class NugetDependencies
+    {
+        [XmlElement(ElementName = "group")]
+        public List<NugetGroup> Groups { get; set; }
+    }
+
+    [Serializable]
+    public class NugetMetadata
+    {
+        [XmlElement("id")]
+        public string Id { get; set; }
+        [XmlElement("version")]
+        public string Version { get; set; }
+        [XmlElement("dependencies")]
+        public NugetDependencies Dependencies { get; set; }
+    }
+
+    [Serializable]
+    [XmlRoot("package", Namespace = "http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd")]
+    public class NugetSpecification2013
+    {
+        [XmlElement("metadata")]
+        public NugetMetadata Metadata { get; set; }
+    }
+
+    [Serializable]
+    [XmlRoot("package", Namespace = "http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd")]
+    public class NugetSpecification2012
+    {
+        [XmlElement("metadata")]
+        public NugetMetadata Metadata { get; set; }
+    }
+}
