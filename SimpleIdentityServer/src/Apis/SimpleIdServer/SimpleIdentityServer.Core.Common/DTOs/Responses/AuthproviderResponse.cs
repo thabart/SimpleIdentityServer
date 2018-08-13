58a7780f02bea759c6a6f14e28c38197508debd4
thabart <habarthierry@hotmail.fr>
2018-08-13 21:08:58 +0200

    Ticket 33 : Return the list of authentication providers

---
+++ SimpleIdentityServer/src/Apis/SimpleIdServer/SimpleIdentityServer.Core.Common/DTOs/Responses/AuthproviderResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace SimpleIdentityServer.Core.Common.DTOs.Responses
+{
+    [DataContract]
+    public class AuthproviderResponse
+    {
+        [DataMember(Name = "display_name")]
+        public string DisplayName { get; set; }
+        [DataMember(Name = "authentication_scheme")]
+        public string AuthenticationScheme { get; set; }
+    }
+}
--- SimpleIdentityServer/src/Apis/SimpleIdServer/SimpleIdentityServer.UserManagement/Extensions/MappingExtensions.cs
+++ SimpleIdentityServer/src/Apis/SimpleIdServer/SimpleIdentityServer.UserManagement/Extensions/MappingExtensions.cs
@@ -1,13 +1,40 @@
-﻿using SimpleIdentityServer.Core.Common.DTOs.Responses;
+﻿using Microsoft.AspNetCore.Authentication;
+using SimpleIdentityServer.Core.Common.DTOs.Responses;
 using SimpleIdentityServer.Core.Common.Models;
 using SimpleIdentityServer.Core.Parameters;
 using SimpleIdentityServer.UserManagement.ViewModels;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SimpleIdentityServer.UserManagement.Extensions
 {
     internal static class MappingExtensions
     {
+        public static AuthproviderResponse ToDto(this AuthenticationScheme authenticationScheme)
+        {
+            if(authenticationScheme == null)
+            {
+                throw new ArgumentNullException(nameof(authenticationScheme));
+            }
+
+            return new AuthproviderResponse
+            {
+                AuthenticationScheme = authenticationScheme.Name,
+                DisplayName = authenticationScheme.DisplayName
+            };
+        }
+
+        public static IEnumerable<AuthproviderResponse> ToDtos(this IEnumerable<AuthenticationScheme> authenticationSchemes)
+        {
+            if (authenticationSchemes == null)
+            {
+                throw new ArgumentNullException(nameof(authenticationSchemes));
+            }
+
+            return authenticationSchemes.Select(a => a.ToDto());
+        }
+
         public static AddUserParameter ToAddUserParameter(this UpdateResourceOwnerCredentialsViewModel updateResourceOwnerViewModel)
         {
             if (updateResourceOwnerViewModel == null)
